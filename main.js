fc77c1ad8a94dcc59210c93c375f22377f8bb2ed
editor-5 <kdch2022@naver.com>
2024-07-02 14:29:23 +0900

    02-02

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
+let taskList =[]
 addButton.addEventListener("click",addTask)
 
 function addTask(){
-    console.log("clicked");
+    let taskContent = taskInput.value;
+    taskList.push(taskContent);
+    console.log(taskList);
 }
  
