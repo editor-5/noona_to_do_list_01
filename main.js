add1e4a542689c079115e7d2fd5165476698ef1e
editor-5 <kdch2022@naver.com>
2024-07-03 16:09:55 +0900

    03-05

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function render(){
           <div class="task-done">${taskList[i].taskContent}</div>
           <div>
             <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-            <button>Delete</button>
+            <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
           </div>
         </div>`;
       }else{
@@ -30,7 +30,7 @@ function render(){
           <div>${taskList[i].taskContent}</div>
           <div>
             <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-            <button>Delete</button>
+            <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
           </div>
         </div>`;
       }
@@ -48,6 +48,16 @@ function toggleComplete(id){
    }
    render();
    console.log(taskList);
+}
+
+function deleteTask(id){
+  for(let i=0;i<taskList.length;i++) {
+    if(taskList[i].id == id){
+      taskList.splice(i,1)
+      break;
+    }
+  }
+  render()  
 }
 
 function randomIDGenerate(){
