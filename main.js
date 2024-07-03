6d451ea284de157e0708481090897320202f11fc
editor-5 <kdch2022@naver.com>
2024-07-03 15:30:08 +0900

    03-02

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ addButton.addEventListener("click",addTask)
 
 function addTask(){
     let task = {
+      id:randomIDGenerate(), 
       taskContent: taskInput.value,
       isComplete:false
     }
@@ -16,14 +17,41 @@ function addTask(){
 function render(){
     let resultHTML = "";
     for(let i=0;i<taskList.length;i++){
+      if(taskList[i].isComplete == true){
         resultHTML += `<div class="task">
-          <div>${taskList[i].taskContent}</div>
+          <div class="task-done">${taskList[i].taskContent}</div>
           <div>
-            <button>Check</button>
+            <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
             <button>Delete</button>
           </div>
         </div>`;
+      }else{
+        resultHTML += `<div class="task">
+          <div>${taskList[i].taskContent}</div>
+          <div>
+            <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+            <button>Delete</button>
+          </div>
+        </div>`;
+      }
+        
     }
     document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+function toggleComplete(id){
+    
+   for(let i=0;i<taskList.length;i++){
+    if(taskList[i].id == id){
+      taskList[i].isComplete = true
+      break;
+    }
+   }
+   render();
+   console.log(taskList);
+}
+
+function randomIDGenerate(){
+  return Math.random().toString(36).substr(2, 16);
 }
  
