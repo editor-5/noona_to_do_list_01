af86283820e286fe2130975ed6c3dc94aa31ccb5
editor-5 <kdch2022@naver.com>
2024-07-04 13:53:59 +0900

    05-01

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
+//유저가 값을 입력한다
+//+ 버튼을 클릭하면, 할일이 추가된다
+// delete버튼을 누르면 할일이 삭제된다
+//check버튼을 누르면 할일이 끝나면서 밑줄이 간다
+//1. check 버튼을 클릭하는순간 true false
+//2. true이면 끝난걸로 간주하고 밑줄 보여주기
+//3. false이면 안끝난걸로 간주하고 그대로
+//진행중 끝남 탭을 누르면, 언더바가 이동한다
+//끝남탭은, 끝남 아이템만, 진행중탭은 진행중인 아이템맘
+//전체탭을 누르면 다시 전체아이템으로 돌아옴
+
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
+let tabs = document.querySelectorAll(".task-tabs div")
 let taskList =[]
 addButton.addEventListener("click",addTask)
 
+for(let i=1;i<tabs.length;i++){
+  tabs[i].addEventListener("click",function(event){
+    filter(event);
+  });
+}
+console.log(tabs)
 function addTask(){
     let task = {
       id:randomIDGenerate(), 
@@ -60,6 +78,9 @@ function deleteTask(id){
   render()  
 }
 
+function filter(event){
+  console.log("filter",event.target.id);
+}
 function randomIDGenerate(){
   return Math.random().toString(36).substr(2, 16);
 }
